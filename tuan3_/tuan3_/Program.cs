48144bf6b775ce929b31b09895304deb1df2b9c4
TTCN123 <127641600+TTCN123@users.noreply.github.com>
2023-04-02 01:13:13 +0700

    object

---
+++ tuan3_/tuan3_/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tuan3
+{
+    class Program
+    {
+        public static int dequy(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * dequy(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("De quy: ");
+            System.Console.WriteLine(dequy(4));
+           
+        }
+    }
+}
