626588e8ebe68d9a925a66637c5716944d5438dd
TTCN123 <127641600+TTCN123@users.noreply.github.com>
2023-04-16 07:46:11 +0700

    database

---
+++ Database/QLSV1.sql
@@ -0,0 +1,3 @@
+﻿select * from tblGiaoVen
+insert into  tblGiaoVen(Ho,TenDem,Ten,GioiTinh,NgaySinh,DiaChi,Email,DienThoai)
+values(N'Trần',N'Văn',N'Hoàng',1,'2002-23-01',N'Thái Nguyên','hoang@gmail.com','01122334455');
